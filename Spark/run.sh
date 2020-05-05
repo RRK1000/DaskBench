e51126fdac8ec5ecf75201c29e549539665247a2
shaanzie <ishaanlagwankar@gmail.com>
2020-05-05 18:14:39 +0530

    Bash script for runs

---
+++ Spark/run.sh
@@ -0,0 +1,25 @@
+# Spark workloads require the following to be installed
+# Spark version >= 1.2
+# Java 8
+# Python 2.7 (initial release) 
+
+# Sort
+# Grep
+# Wordcount
+
+python3 Wordcount.py ../Data/Lorem.txt
+
+# MD5
+# Connected Components
+# RandSample
+# FFT
+# Matrix Multiplication
+# Read
+# Write
+# Scan
+# OrderBy
+# Aggregation
+# Project
+# Filter
+# Select
+# Union
